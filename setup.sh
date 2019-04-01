5a4d6b9b3971c81c1ad07f6dc3386d14b1985171
joachimth <joachim@thirsbro.dk>
2019-04-01 17:33:11 +0200

    new file:   Makefile 	new file:   RoadApplePi.ino 	new file:   dnsmasq.conf 	new file:   hostapd-rap.conf 	new file:   html/.htaccess 	new file:   html/dataHandler.php 	new file:   html/fontawesome-all.js 	new file:   html/icon.png 	new file:   html/index.php 	new file:   html/justgage.js 	new file:   html/localforage.min.js 	new file:   html/logo.svg 	new file:   html/manifest.json 	new file:   html/opensans.css 	new file:   html/opensans/59ZRklaO5bWGqF5A9baEERJtnKITppOI_IvcXXDNrsc.woff2 	new file:   html/opensans/LWCjsQkB6EMdfHrEVqA1KRJtnKITppOI_IvcXXDNrsc.woff2 	new file:   html/opensans/RjgO7rYTmqiVp7vzi-Q5URJtnKITppOI_IvcXXDNrsc.woff2 	new file:   html/opensans/cJZKeOuBrn4kERxqtaUH3VtXRa8TVwTICgirnJhmVJw.woff2 	new file:   html/opensans/u-WUoqrET9fUeobQW7jkRRJtnKITppOI_IvcXXDNrsc.woff2 	new file:   html/opensans/xozscpT2726on7jbcb_pAhJtnKITppOI_IvcXXDNrsc.woff2 	new file:   html/raphael-2.1.4.min.js 	new file:   html/roadapplepi.appcache 	new file:   html/script.js 	new file:   html/styles.css 	new file:   raprec.c 	new file:   raprec.service 	new file:   raprun.c 	new file:   roadapplepi.sql 	new file:   setup.sh

---
+++ setup.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+softwareVersion=$(git describe --long)
+
+echo -e "\e[1;4;246mRoadApplePi Setup $softwareVersion\e[0m
+Welcome to RoadApplePi setup. RoadApplePi is \"Black Box\" software that
+can be retrofitted into any car with an OBD port. This software is meant
+to be installed on a Raspberry Pi running unmodified Raspbian Stretch,
+but it may work on other OSs or along side other programs and modifications.
+Use with anything other then out-of-the-box Vanilla Raspbain Stretch is not
+supported.
+
+This script will download, compile, and install the necessary dependencies
+before finishing installing RoadApplePi itself. Depending on your model of
+Raspberry Pi, this may take several hours.
+"
+
+#Prompt user if they want to continue
+read -p "Would you like to continue? (y/N) " answer
+if [ "$answer" == "n" ] || [ "$answer" == "N" ] || [ "$answer" == "" ]
+then
+	echo "Setup aborted"
+	exit
+fi
+
+#################
+# Update System #
+#################
+echo -e "\e[1;4;93mStep 1. Updating system\e[0m"
+sudo apt update
+sudo apt upgrade -y
+
+###########################################
+# Install pre-built dependencies from Apt #
+###########################################
+echo -e "\e[1;4;93mStep 2. Install pre-built dependencies from Apt\e[0m"
+sudo apt install -y dnsmasq hostapd libbluetooth-dev apache2 php7.0 php7.0-mysql php7.0-bcmath mariadb-server libmariadbclient-dev libmariadbclient-dev-compat uvcdynctrl
+sudo systemctl disable hostapd dnsmasq
+
+################
+# Build FFMpeg #
+################
+echo -e "\e[1;4;93mStep 3. Build ffmpeg (this may take a while)\e[0m"
+ffmpegLocation=$(which ffmpeg)
+if [ $? != 0 ]
+then
+	wget https://www.ffmpeg.org/releases/ffmpeg-3.4.2.tar.gz
+	tar -xvf ffmpeg-3.4.2.tar.gz
+	cd ffmpeg-3.4.2
+	echo "./configure --enable-gpl --enable-nonfree --enable-mmal --enable-omx --enable-omx-rpi"
+	./configure --enable-gpl --enable-nonfree --enable-mmal --enable-omx --enable-omx-rpi
+	make -j$(nproc)
+	sudo make install
+else
+	echo "FFMpeg already found at $ffmpegLocation! Using installed version."
+fi
+
+#######################
+# Install RoadApplePi #
+#######################
+echo -e "\e[1;4;93mStep 4. Building and installing RoadApplePi\e[0m"
+cd ..
+make
+sudo make install
+
+sudo cp -r html /var/www/
+sudo rm /var/www/html/index.html
+sudo chown -R www-data:www-data /var/www/html
+sudo chmod -R 0755 /var/www/html
+sudo cp raprec.service /lib/systemd/system
+sudo chown root:root /lib/systemd/system/raprec.service
+sudo chmod 0755 /lib/systemd/system/raprec.service
+sudo systemctl daemon-reload
+sudo systemctl enable raprec
+sudo cp hostapd-rap.conf /etc/hostapd
+sudo cp dnsmasq.conf /etc
+sudo mkdir /var/www/html/vids
+sudo chown -R www-data:www-data /var/www/html
+
+installDate=$(date)
+cp roadapplepi.sql roadapplepi-configd.sql
+echo "INSERT INTO env (name, value) VALUES (\"rapVersion\", \"$softwareVersion\"), (\"installDate\", \"$installDate\");" >> roadapplepi-configd.sql
+sudo mysql < roadapplepi-configd.sql
+
+echo "Done! Please reboot your Raspberry Pi now"
