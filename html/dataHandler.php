0bd3f424c6cfc73a03d35d75cb47f2b83587a446
joachimth <46874512+joachimth@users.noreply.github.com>
2019-04-14 00:39:50 +0200

    Update dataHandler.php

--- html/dataHandler.php
+++ html/dataHandler.php
@@ -239,41 +239,6 @@ if(in_array($_GET['action'], array('ap', 'ct')))
 	exit();
 }
 
-//Discover USB/Serial devices - JT2019
-if($_GET['action'] == 'cablediscover')
-{
-	ob_start();
-	system("lsusb", $retStatus);
-	$returnedJson = ob_get_clean();
-	if($retStatus == 0) echo $returnedJson;
-	else
-	echo 'Big fail';
-	exit;
-}
-
-//Pair USB/Serial devices - JT2019
-if($_GET['action'] == 'cbpair')
-{
-	ob_start();
-	#system("raprun -p ".$_POST['mac']." ".$_POST['pin'], $retStatus);
-	system("lsusb", $retStatus);
-	$exitText = ob_get_clean();
-	
-	if($retStatus == 0)
-	{
-		$toJS = array(true, $exitText);
-		
-		$mysqli = new mysqli('127.0.0.1', 'roadapplepi', 'roadapplepi', 'roadapplepi');
-		$mysqli->query("delete from env where name='currentOBD'");
-		$mysqli->query("insert into env (name, value) values ('currentOBD', '".$mysqli->real_escape_string('Serial')."')");
-		$mysqli->close();
-	}
-	else $toJS = array(false, $exitText);
-	
-	echo json_encode($toJS);
-	exit();
-}
-
 //Discover bluetooth devices
 if($_GET['action'] == 'discover')
 {
